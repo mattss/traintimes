37f97030d838fb3b3ea05e2b853cbf9f625211d2
Matthew Sital-Singh <mattss@netsight.co.uk>
2018-06-21 16:35:33 +0100

    Allow source/dest to be passed in

--- trains.py
+++ trains.py
@@ -1,8 +1,19 @@
+import sys
+
 import requests
 
-api_url = 'https://fcc.transportapi.com/v3/uk/train/station/BRI/live.json?limit=10&calling_at=BPW'
+api_url = 'https://fcc.transportapi.com/v3/uk/train/station/{source}/live.json?limit=10&calling_at={destination}'
 
-result = requests.get(api_url)
+try:
+    source, destination = sys.argv[1:]
+except ValueError:
+    print('Please provide a source and destination')
+    sys.exit(1)
+
+result = requests.get(api_url.format(
+    source=source,
+    destination=destination,
+))
 
 data = result.json()
 print(data['station_name'])
