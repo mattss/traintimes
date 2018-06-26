5ee2dae842c5ca348b63b2a80a6cba386e0fe3b7
Matthew Sital-Singh <mattss@netsight.co.uk>
2018-06-26 19:54:03 +0100

    Catch connection errors

--- trains.py
+++ trains.py
@@ -1,8 +1,12 @@
 import sys
 
 import requests
+from requests.exceptions import ConnectionError
 
-api_url = 'https://fcc.transportapi.com/v3/uk/train/station/{source}/live.json?limit=10&calling_at={destination}'
+api_url = (
+    'https://fcc.transportapi.com/v3/uk/train/station/{source}/live.json'
+    '?limit=10&calling_at={destination}'
+    )
 
 try:
     source, destination = sys.argv[1:]
@@ -10,13 +14,18 @@ except ValueError:
     print('Please provide a source and destination')
     sys.exit(1)
 
-result = requests.get(api_url.format(
-    source=source,
-    destination=destination,
-))
+try:
+    result = requests.get(api_url.format(
+        source=source,
+        destination=destination,
+    ))
+except ConnectionError:
+    print('<connerror>')
+    sys.exit(1)
 
 data = result.json()
 print(data['station_name'])
 
 for result in data['departures']['all']:
-    print('{aimed_departure_time} // {status} - {destination_name}'.format(**result))
+    print('{aimed_departure_time} // {status} - {destination_name}'.format(
+        **result))
